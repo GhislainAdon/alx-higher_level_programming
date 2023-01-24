ffb0b437bdbcfab29774001c575a83663cc9d8e4
GhislainAdon <esaticsrit1b@gmail.com>
2023-01-24 15:43:53 -0800

    0x06-python-classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module providing an empty definition of a class 'Square'
+"""
+
+
+class Square():
+    """ Empty definition of a 'Square'
+    """
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Module providing a definition of a class 'Square'
+"""
+
+
+class Square():
+    """ Definition of a 'Square'
+    """
+    def __init__(self, size=0):
+        """ Instantiate a 'Square'
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
