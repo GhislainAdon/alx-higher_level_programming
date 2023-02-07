509b15b39cbff8dda3354bc221c011ded2b6a471
GhislainAdon <esaticsrit1b@gmail.com>
2023-02-07 09:58:54 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -10,6 +10,14 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Get a JSON representation of a student"""
+        if isinstance(attrs, list) and all(
+                map(lambda s: isinstance(s, str), attrs)
+        ):
+            return {k: self.__dict__[k] for k in attrs if k in self.__dict__}
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """Load data from a JSON representation of a student"""
+        self.__dict__.update(json)
